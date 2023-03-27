6c3d5228157d9b6f4d984e21d76ecea9289ced17
Matheus Julian Van Dyke <111263179+matheus-vandyke@users.noreply.github.com>
2023-03-27 03:19:40 -0300

    start of integration

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navBar() {
-    var x = document.getElementById("myNavBar");
-    if (x.className === "navbar") {
-        x.className += "responsive";
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-        x.className = "navbar";
-    };
-};
+      x.className = "topnav";
+    }
+  }
